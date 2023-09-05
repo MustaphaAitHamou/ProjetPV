6c371bd02fa1146807c1424ce905dd672195529d
Mustapha Ait-Hamou <mustaphaithamou@gmail.com>
2023-09-05 02:23:50 +0200

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('./connection')
 const server = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server, {
-  cors: 'http://localhost:3001',
+  cors: 'http://https://www.pure-view.fr',
   methods: ['GET', 'POST', 'PATCH', "DELETE"]
 })
 
